1238b8e598df3c7b1e92d8f4a4e81d7f72b10fa7
Noah8922 <juyeolgo@gmail.com>
2022-03-29 20:46:09 +0900

    2.functions-optional parameter

--- class-note/2_functions.ts
+++ class-note/2_functions.ts
@@ -15,7 +15,13 @@ function sum(a:number,b:number): number {
     return a+b
 }
 
-sum(10,20) //이상적인 상태
+sum(10,20) //이상적인 상태, 파라미터가 적어도 안되고 많아도 안됨.
 // sum(10,20,30,40) 
 // 오류가 나는 상태 : 2개의 인수가 필요한데 4개를 가져왔습니다.
 
+//함수의 옵셔널 파라미, 인자의 개수를 자유롭게 하고 싶을 때, 필요에 따라 인자를 넘길수도 있고 아닐수도 있다.
+function log(a:string, b?:string) {
+
+}
+log("asd")
+log('asd', "asd")
