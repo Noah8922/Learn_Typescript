8ebf460ca6ba9eeddb75c5979bde36b721f13188
Noah8922 <juyeolgo@gmail.com>
2022-06-24 08:38:10 +0900

    Union type의 장점

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -9,8 +9,24 @@ logMessage("hello")
 /** logMessage(100) // string이 아니기 때문에 에러남 */
 
 /** 유니온 타입 쓴경우 */
+/** 파라미터에 한가지 이상의 타입을 쓰고 싶을 때 사용 */
+let seho : string | number | boolean;
 function showmessage(value : string | number) {
     console.log(value)
 }
 showmessage('hello')
 showmessage(100)
+
+/** 유니온 타입의 장점 */
+/** 타입에 따른 API들을 바로 사용 할 수 있다. */
+/** 타입가드 : 특정 타입으로 타입의 범위를 좁혀나가는 (필터링 하는) 과정 */
+
+function filterType(value : string | number) {
+    if (typeof value === 'string') {
+        value.toString()
+    }
+    if (typeof value === 'number') {
+        value.toLocaleString()
+    }
+    throw new TypeError('Type must be string or number')
+}
