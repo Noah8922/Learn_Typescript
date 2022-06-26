be9b5315693c3b4b2235adc7a59827accee2ba01
Noah8922 <juyeolgo@gmail.com>
2022-06-26 22:11:45 +0900

    제너릭 예시

--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,26 +1,50 @@
-const emails = [
+// 이렇게 하나 만들어 놓으면 다른게 필요가 없다.
+interface Dropdown<T> {
+  value : T;
+  selected : boolean; 
+}
+
+// interface Email {
+//   value : string;
+//   selected : boolean;
+// }
+
+// interface TrueFalse {
+//   value : boolean;
+//   selected : boolean
+// }
+
+const emails:Dropdown<string>[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-const numberOfProducts = [
+// interface ProductNumber {
+//   value : number;
+//   selected : boolean;
+// }
+const numberOfProducts: Dropdown<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item) {
+function createDropdownItem<T>(item:Dropdown<T>) {
   const option = document.createElement('option');
-  option.value = item.value.toString();
-  option.innerText = item.value.toString();
+  // option.value = item.value.toString();
+  // option.innerText = item.value.toString();
   option.selected = item.selected;
   return option;
 }
 
 // NOTE: 이메일 드롭 다운 아이템 추가
 emails.forEach(function (email) {
-  const item = createDropdownItem(email);
+  const item = createDropdownItem<string>(email);
   const selectTag = document.querySelector('#email-dropdown');
-  selectTag.appendChild(item);
+  // selectTag.appendChild(item);
 });
+
+numberOfProducts.forEach(function (product) {
+  const item = createDropdownItem<number>(product)
+})
